8263986de2b2113d0277cba278dfd24635915d65
Sebas Corral <sebascorralbrito@gmail.com>
2022-02-05 00:28:30 +0100

    iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+import { useState } from 'react';
 import './App.css';
+import contactsJSON from "./contacts.json";
+
 
 function App() {
-  return   <div className="App">
+  const [contacts, setContacts] = useState(contactsJSON.slice(0, 5));
 
-      </div>;
+  const sortName = () => {
+    const sortedName = [...contacts].sort(function (a, b) {
+      if (b.name > a.name) {
+        return -1;
+      }
+      return 0;
+    });
+    setContacts(sortedName);
+  }
+
+  return <div className="App">
+    <h2 className='header'>IronContacts</h2>
+  
+    <table className='contacts-table'>
+      <th>Picture</th>
+      <th>Name</th>
+      <th>Popularity</th>
+      
+      {contacts.map((celebrity) => {
+        return (
+          <tr>
+            <td>
+              <img src={celebrity.pictureUrl} alt='profile_picture' width='70px' />
+            </td>
+            <td>{celebrity.name}</td>
+            <td>{celebrity.popularity.toFixed(2)}</td>
+          </tr>
+        )
+      })}
+    </table>
+  </div>
 }
 
 export default App;
